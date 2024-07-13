0c630f69e8ec4a1e5a87bf071501b29c852b2737
elskow <elskow@outlook.com>
2024-07-14 00:54:00 +0700

    feat [accounts,cards,loans]: migrating to more proper database (postgres), also adjusting the schema

--- accounts/src/main/resources/schema.sql
+++ accounts/src/main/resources/schema.sql
@@ -1,22 +1,31 @@
-CREATE TABLE IF NOT EXISTS `customers`
+-- Create sequences
+CREATE SEQUENCE customers_customer_id_seq;
+CREATE SEQUENCE accounts_account_number_seq;
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS "customers"
 (
-    `customer_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name`        VARCHAR(255) NOT NULL,
-    `nik`         VARCHAR(16)  NOT NULL,
-    `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `created_by`  VARCHAR(255) NOT NULL,
-    `updated_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_by`  VARCHAR(255) NOT NULL
+    "customer_id" INT          NOT NULL    DEFAULT nextval('customers_customer_id_seq') PRIMARY KEY,
+    "name"        VARCHAR(255) NOT NULL,
+    "nik"         VARCHAR(16)  NOT NULL,
+    "created_at"  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "created_by"  VARCHAR(255) NOT NULL,
+    "updated_at"  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_by"  VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `accounts`
+CREATE TABLE IF NOT EXISTS "accounts"
 (
-    `customer_id`    INT          NOT NULL,
-    `account_number` INT AUTO_INCREMENT PRIMARY KEY,
-    `account_type`   VARCHAR(50)  NOT NULL,
-    `branch_address` VARCHAR(255) NOT NULL,
-    `created_at`     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `created_by`     VARCHAR(255) NOT NULL,
-    `updated_at`     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_by`     VARCHAR(255) NOT NULL
+    "customer_id"    INT          NOT NULL,
+    "account_number" INT          NOT NULL    DEFAULT nextval('accounts_account_number_seq') PRIMARY KEY,
+    "account_type"   VARCHAR(50)  NOT NULL,
+    "branch_address" VARCHAR(255) NOT NULL,
+    "created_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "created_by"     VARCHAR(255) NOT NULL,
+    "updated_at"     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_by"     VARCHAR(255) NOT NULL
 );
+
+-- Alter sequences to set ownership
+ALTER SEQUENCE customers_customer_id_seq OWNED BY customers.customer_id;
+ALTER SEQUENCE accounts_account_number_seq OWNED BY accounts.account_number;
--- cards/src/main/resources/schema.sql
+++ cards/src/main/resources/schema.sql
@@ -1,15 +1,14 @@
-CREATE TABLE IF NOT EXISTS `cards`
+CREATE TABLE IF NOT EXISTS "cards"
 (
-    `card_id`          int          NOT NULL AUTO_INCREMENT,
-    `nik`              varchar(16)  NOT NULL,
-    `card_number`      varchar(100) NOT NULL,
-    `card_type`        varchar(100) NOT NULL,
-    `total_limit`      int          NOT NULL,
-    `amount_used`      int          NOT NULL,
-    `available_amount` int          NOT NULL,
-    `created_at`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `created_by`       VARCHAR(255) NOT NULL,
-    `updated_at`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `updated_by`       VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`card_id`)
+    "card_id"          SERIAL PRIMARY KEY,
+    "nik"              VARCHAR(16)  NOT NULL,
+    "card_number"      VARCHAR(100) NOT NULL,
+    "card_type"        VARCHAR(100) NOT NULL,
+    "total_limit"      INT          NOT NULL,
+    "amount_used"      INT          NOT NULL,
+    "available_amount" INT          NOT NULL,
+    "created_at"       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "created_by"       VARCHAR(255) NOT NULL,
+    "updated_at"       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_by"       VARCHAR(255) NOT NULL
 );
